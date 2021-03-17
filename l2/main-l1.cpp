ed9d48d1f7a2a6091d900c02e84b043745d73ad5
Hanyuan Ye <h43ye@edu.uwaterloo.ca>
2021-03-16 20:02:30 -0400

    lab 2 persistent

---
+++ l2/main-l1.cpp
@@ -0,0 +1,369 @@
+#include <iostream>
+#include <stdlib.h>
+#include <random>
+#include <math.h>
+#include <vector>
+#include <deque>
+#include <functional>
+#include <algorithm>
+#include <string>
+#include <fstream>
+#include <chrono>
+
+enum EventType { ARRIVAL, DEPARTURE, OBSERVER };
+
+class Event {
+public:
+    double timestamp;
+    double serviceTime;
+    EventType type;
+
+    Event(double t_timestamp, double t_serviceTime, EventType t_type) {
+        timestamp = t_timestamp;
+        serviceTime = t_serviceTime;
+        type = t_type;
+    }
+
+    Event(double t_timestamp, EventType t_type) {
+        timestamp = t_timestamp;
+        serviceTime = 0;
+        type = t_type;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Event& event) {
+        os << event.timestamp << "/" << event.serviceTime;
+        return os;
+    }
+};
+
+struct Result {
+    double rho;
+    double packetLoss;
+    double queueSizeTotal;
+    double idleTimeTotal;
+
+    Result() {
+        rho = 0;
+        packetLoss = 0;
+        queueSizeTotal = 0;
+        idleTimeTotal = 0;
+    };
+};
+
+double startRho = 0.25;
+double endRho = 0.95;
+double incrementRho = 0.10;
+double arrivalRatio = 500;
+double T = 1000;
+double queueSize = 0;
+double arrivalLambda = 0;
+double lengthLambda = 0.0005;
+double c = 1000000;
+std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());;
+
+std::uniform_real_distribution<double> distribution(0.0, 1.0);
+
+void printResults(Result result) {
+        std::cout  << "Rho: " << result.rho << ", Packet loss: " << result.packetLoss << ", Queue Size: " << result.queueSizeTotal << ", idleTimeTotal: " << result.idleTimeTotal << std::endl;
+}
+
+double exponentialValue(double lambda, double uniform) {
+    return -(1 / lambda) * log(1 - uniform); 
+}
+
+std::vector<Event> generateArrivals(int simulationTime) {
+    double currTime = 0;
+
+    std::vector<Event> arrivalEvents;
+    while (currTime < simulationTime) {
+        double nextArrival = exponentialValue(arrivalLambda, distribution(generator));
+        double serviceTime = exponentialValue(lengthLambda, distribution(generator)) / c;
+        Event newEvent(nextArrival + currTime, serviceTime, ARRIVAL);
+        arrivalEvents.push_back(newEvent);
+        currTime = nextArrival + currTime;
+     }
+
+    return arrivalEvents;
+}
+
+std::vector<Event> generateDepartures(std::vector<Event> arrivals, int simulationTime) {
+    double currTime = 0;
+    std::vector<Event> departures;
+
+    for (auto a: arrivals) {
+        double departureTime = 0;
+
+        if (a.timestamp <= currTime) {
+            departureTime = currTime + a.serviceTime;
+        } else {
+            departureTime = a.timestamp + a.serviceTime;
+        }
+
+        currTime = departureTime;
+
+        departures.push_back(Event(departureTime, DEPARTURE));
+    }
+
+    return departures;
+}
+
+std::vector<Event> generateDepartures(std::vector<Event> arrivals, int simulationTime, int queueSize) {
+
+    if (queueSize == 0) {
+        return generateDepartures(arrivals, simulationTime);
+    }
+
+    std::deque<Event> eventQueue;
+
+    for (auto c: arrivals) {
+        eventQueue.push_back(c);
+    }
+
+    std::vector<Event> departures;
+    std::deque<Event> packetQueue;
+
+    double currTime = 0;
+    // Simulate queue to see which packets are serviced
+    // While packets in the packet queue can be serviced before next arrival
+    // service them.
+    //
+    // On the next arrival event, attempt to add it to the packet queue.
+    // If full, skip it and increment packet loss counter, otherwise add it.
+
+    while (currTime < simulationTime && (eventQueue.size() > 0 || packetQueue.size() > 0)) {
+        // Service packets in packet queue
+        while (packetQueue.size() > 0 && (eventQueue.size() == 0 || packetQueue.front().serviceTime + currTime < eventQueue.front().timestamp)) {
+            currTime += packetQueue.front().serviceTime;
+            departures.push_back(Event(currTime, DEPARTURE));
+            packetQueue.pop_front();
+        }
+
+        if (eventQueue.size() == 0) { continue; }
+
+        Event next = eventQueue.front();
+        eventQueue.pop_front();
+
+        // packet loss
+        if (packetQueue.size() == queueSize) {
+            continue;
+        }
+
+        // add next packet to packet queue
+        packetQueue.push_back(next);
+        if (packetQueue.size() == 1) {
+            currTime = packetQueue.front().timestamp;
+        }
+    }
+
+    return departures;
+}
+
+std::vector<Event>generateObservers(int simulationTime) {
+    double currTime = 0;
+
+    std::vector<Event> observerEvents;
+    while (currTime < simulationTime) {
+        double nextArrival = exponentialValue(arrivalLambda*5.0, distribution(generator));
+        Event newEvent(nextArrival + currTime, OBSERVER);
+        observerEvents.push_back(newEvent);
+        currTime = nextArrival + currTime;
+     }
+
+    return observerEvents; 
+}
+
+Result runDes(std::vector<Event> events, int simulationTime, int size, int arrivalsSize, int observersSize) {
+    std::deque<Event> queue;
+
+    for (auto e: events) {
+        queue.push_back(e);
+    }
+
+    Result result;
+    int currentQueueSize = 0;
+    double previousTime = 0;
+
+    int departureCount = 0;
+    int arrivalCount = 0;
+    while (queue.size() > 0) {
+        auto event = queue.front();
+        queue.pop_front();
+
+        if (event.timestamp >= simulationTime) { break; }
+
+        switch (event.type) {
+        case ARRIVAL:
+            if (size > 0 && currentQueueSize == size) {
+                result.packetLoss += 1;
+            } else {
+                arrivalCount += 1;
+                currentQueueSize += 1;
+            }
+            break;
+        case DEPARTURE:
+            currentQueueSize -= 1;
+            departureCount += 1;
+            break;
+        case OBSERVER:
+            result.queueSizeTotal += currentQueueSize;
+
+            if (currentQueueSize == 0) {
+                result.idleTimeTotal += 1;
+            }
+            break;
+        }
+
+        previousTime = event.timestamp; 
+    }
+
+    result.packetLoss /= arrivalsSize;
+    result.queueSizeTotal /= observersSize;
+    result.idleTimeTotal /= observersSize;
+
+    return result;
+}
+
+bool isStable(double v1, double v2) {
+    if (v2 < 0.005) { return true; }
+
+    return abs((v1 - v2) / v2) < 0.04;
+}
+
+bool isStable(Result r1, Result r2) {
+    return isStable(r1.packetLoss, r2.packetLoss) &&
+           isStable(r1.queueSizeTotal, r2.queueSizeTotal) &&
+           isStable(r1.idleTimeTotal, r2.idleTimeTotal);
+}
+
+void outputGraphTxt1(std::vector<Result> results, std::string filename) {
+    std::ofstream txtOut;
+    txtOut.open(filename, std::ofstream::out | std::ofstream::trunc);
+    for (auto result: results) {
+        txtOut << result.rho << " " << result.queueSizeTotal << std::endl;
+    }
+    txtOut.close();
+}
+
+void outputGraphTxt2(std::vector<Result> results, std::string filename) {
+    std::ofstream txtOut;
+    txtOut.open(filename, std::ofstream::out | std::ofstream::trunc);
+    for (auto result: results) {
+        txtOut << result.rho << " " << result.idleTimeTotal << std::endl;
+    }
+    txtOut.close();
+}
+
+void outputGraphTxt3(std::vector<Result> results, std::string filename, bool shouldTruncate) {
+    std::ofstream txtOut;
+    if (shouldTruncate) {
+        txtOut.open(filename, std::ofstream::out | std::ofstream::trunc);
+    } else {
+        txtOut.open(filename, std::ofstream::out | std::ofstream::app);    
+        txtOut << std::endl;
+        txtOut << std::endl;
+    }
+    for (auto result: results) {
+        txtOut << result.rho << " " << result.packetLoss << std::endl;
+    }
+    txtOut.close();
+}
+
+void outputGraphTxt4(std::vector<Result> results, std::string filename, bool shouldTruncate) {
+    std::ofstream txtOut;
+    if (shouldTruncate) {
+        txtOut.open(filename, std::ofstream::out | std::ofstream::trunc);
+    } else { 
+        txtOut.open(filename, std::ofstream::out | std::ofstream::app);
+        txtOut << std::endl;
+        txtOut << std::endl;
+    }
+    for (auto result: results) {
+        txtOut << result.rho << " " << result.queueSizeTotal << std::endl;
+    }
+    txtOut.close();
+}
+
+std::vector<Result> runSimulation() {
+    bool stable = false;
+    Result prevResult;
+    std::vector<Result> results;
+    T = 1000;
+
+    while (!stable) {
+        double rho = startRho;
+        results.clear();
+        while (rho <= endRho + 0.05) {
+            arrivalLambda = rho * arrivalRatio;
+            auto arrivals = generateArrivals(T);
+            auto departures = generateDepartures(arrivals, T, queueSize);
+            auto observers = generateObservers(T);
+
+            std::vector<Event> events = arrivals;
+            events.insert(events.end(), departures.begin(), departures.end());
+            events.insert(events.end(), observers.begin(), observers.end());
+
+            std::sort(std::begin(events),
+                      std::end(events),
+                      [](Event a, Event b) { return a.timestamp < b.timestamp; });
+            
+            int count = 0;
+            for (auto event: events) {
+                std::string str;
+                if (event.type == ARRIVAL) {
+                    count += 1;
+                    str = "Arrival";
+                } else if (event.type == DEPARTURE) {
+                    count -= 1;
+                    str = "Departure";
+                }
+
+           //     std::cout << "count: " << count << " timestamp: " << event.timestamp << " type: " << str << std::endl;
+            }
+            auto result = runDes(events, T, queueSize, arrivals.size(), observers.size());
+            result.rho = rho;
+            printResults(result);
+            results.push_back(result);
+            if (rho > endRho - 0.05) {
+                stable = isStable(prevResult, result);
+                std::cout << "Queue Size: " << queueSize << ", T: " << T << ", stable: " << stable << std::endl;
+                printResults(result);
+                prevResult = result;
+            }
+
+            rho += incrementRho;
+        }
+
+        T += 1000;
+    }
+    return results;
+}
+
+void runSimulation(int t_queueSize) {
+    queueSize = t_queueSize;
+    auto results = runSimulation();
+    outputGraphTxt3(results, "q6_dataPacketLoss", t_queueSize == 10);
+    outputGraphTxt4(results, "q6_dataEn", t_queueSize == 10);
+}
+
+int main(int argc, char* argv[]) {
+
+    int mode = strtol(argv[1], NULL, 10);
+    if (mode == 0) {
+        startRho = 0.25;
+        endRho = 0.95;
+    } else {
+        startRho = 0.5;
+        endRho = 1.5;
+    }
+
+    if (mode == 1) {
+        runSimulation(10);
+        runSimulation(25);
+        runSimulation(50);
+    } else {
+        auto results = runSimulation();
+        outputGraphTxt1(results, "q3_data1");
+        outputGraphTxt2(results, "q3_data2");
+    }
+}
+
---
+++ l2/main.cpp
@@ -0,0 +1,230 @@
+#include <iostream>
+#include <stdlib.h>
+#include <random>
+#include <math.h>
+#include <vector>
+#include <deque>
+#include <functional>
+#include <algorithm>
+#include <string>
+#include <fstream>
+#include <chrono>
+
+double T = 1000;
+double c = 3 * pow(10, 8);
+double T_PROP = 10.0 / (2.0 / 3.0 * c);
+double T_TRANS = 1500.0 / 1000000.0;
+
+int transmissionAttempts = 0;
+int transmitted = 0;
+
+std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());;
+std::uniform_real_distribution<double> distribution(0.0, 1.0);
+
+
+// Utils
+double exponentialValue(double lambda) {
+    double uniform = distribution(generator);
+    return -(1 / lambda) * log(1 - uniform);
+}
+
+double backoff(int collisions) {
+    std::uniform_int_distribution<int> int_distribution(0, pow(2, collisions));
+    int randVal = int_distribution(generator);
+    return (double)randVal * 512.0 / 1000000.0;
+}
+
+class Result {
+public:
+    int a;
+    int n;
+    double efficiency;
+    double throughput;
+
+    Result(double t_efficiency, double t_throughput) {
+        efficiency = t_efficiency;
+        throughput = t_throughput;
+        a = 0;
+        n = 0;
+    }
+};
+
+class Node {
+public:
+    int lambda;
+    int pos;
+    int collisionCount;
+    double nextFrame;
+
+    Node(int t_lambda, int t_pos) {
+        lambda = t_lambda;
+        pos = t_pos;
+        collisionCount = 0;
+        nextFrame = exponentialValue(lambda);
+    }
+
+    void handleCollision() {
+        collisionCount += 1;
+        if (collisionCount > 10) {
+            collisionCount = 0;
+            nextFrame += exponentialValue(lambda);
+        } else {
+            nextFrame += backoff(collisionCount);
+        }
+    }
+
+    void senderCollision(double max_delay) {
+        handleCollision();
+        nextFrame = max_delay;
+    }
+
+    void senseBusy(double start, double end) {
+        if (start <= nextFrame && nextFrame < end) {
+            nextFrame = end;
+        }
+    }
+
+    void sendSuccessfully() {
+        collisionCount = 0;
+        nextFrame += exponentialValue(lambda);
+    }
+};
+
+std::vector<Node> generateNodes(int avgPackets, int n) {
+    std::vector<Node> nodes; 
+    for (int i = 0; i < n; i++) {
+        nodes.push_back(Node(avgPackets, i));
+    }
+
+    return nodes;
+}
+
+Result simulate(double simulationTime, std::vector<Node> nodes) {
+
+    double timer = 0;
+
+    while (timer < simulationTime) {
+        // Retrieve next event
+        int minIdx = 0;
+        for (int i = 0; i < nodes.size(); i++) {
+            if (nodes[i].nextFrame < nodes[minIdx].nextFrame) {
+                minIdx = i;
+            }
+        }
+        Node &minNode = nodes[minIdx];
+
+        timer = minNode.nextFrame;
+        // check collisions
+        int maxCollidingDistance = -1;
+
+        transmissionAttempts ++;
+
+        for (auto &node: nodes) {
+            if (node.pos == minNode.pos) { continue; }
+
+            int distance = abs(minNode.pos - node.pos);
+            double sendingTime = minNode.nextFrame;
+            double arrivalTime = sendingTime + T_PROP * distance;
+            double lastBit = arrivalTime + T_TRANS;
+
+            // collision case
+            if (node.nextFrame < arrivalTime) {
+                maxCollidingDistance = std::max(distance, maxCollidingDistance);
+                node.handleCollision();  
+                transmissionAttempts ++;
+            } else {
+                node.senseBusy(arrivalTime, lastBit);
+            }
+        }
+
+        if (maxCollidingDistance >= 0) {
+            minNode.senderCollision(minNode.nextFrame + T_TRANS + maxCollidingDistance * T_PROP); 
+        } else {
+            transmitted ++;
+            //std::cout << transmitted << " " << minNode.nextFrame << " " << transmissionAttempts << std::endl;
+            minNode.sendSuccessfully(); 
+        }
+    }
+
+    double efficiency = (double)transmitted / (double)transmissionAttempts;
+    double throughput = (double)transmitted * T_TRANS / simulationTime;
+    return Result(efficiency, throughput);
+}
+
+Result createSimulation(int avgPackets, int numNodes) {
+    transmitted = 0;
+    transmissionAttempts = 0;
+    auto nodes = generateNodes(avgPackets, numNodes);
+
+    auto result = simulate(T, nodes);
+    result.a = avgPackets;
+    result.n = numNodes;
+
+    std::cout << T << " " << result.a << " " << result.n << " " << result.efficiency << " " << result.throughput << std::endl;
+    return result; 
+}
+
+bool isStable(double v1, double v2) {
+    if (v2 < 0.005) { return true; }
+
+    return abs((v1 - v2) / v2) < 0.04;
+}
+
+bool isStable(Result r1, Result r2) {
+    return isStable(r1.efficiency, r2.efficiency) && isStable(r1.throughput, r2.throughput);    
+}
+
+void clear(std::string filename) {
+    std::ofstream ofs;
+    ofs.open(filename, std::ofstream::out | std::ofstream::trunc);
+    ofs.close();
+}
+
+void write(std::vector<Result> results, std::string filename) {
+    std::ofstream txtOut;
+    txtOut.open(filename + "Ef", std::ofstream::out | std::ofstream::trunc);
+    for (auto result: results) {
+        txtOut << result.n << " " << result.efficiency << std::endl;
+    }
+    txtOut.close();
+
+    txtOut.open(filename + "Th", std::ofstream::out | std::ofstream::trunc);
+    for (auto result: results) {
+        txtOut << result.n << " " << result.throughput << std::endl;
+    }
+    txtOut.close();
+}
+
+int main() {
+    clear("persistentEf");
+    clear("persistentTh");
+
+    std::vector<int> A {7, 10, 20};
+    std::vector<int> N {20, 40, 60, 80, 100};
+
+    auto prev = Result(0, 0);
+    std::vector<Result> results;
+    while(1) {
+        for (auto a: A) {
+            for (auto n: N) {
+                auto result = createSimulation(a, n);
+                results.push_back(result);
+                if (result.a == 20 && result.n == 100) {
+                    if (isStable(prev, result)) {
+                        write(results, "persistent");
+                        std::cout << "Stable" << std::endl;
+                        return 0;
+                    } else {
+                        results.clear();
+                        prev = result;
+                        T += 1000;
+                        std::cout << "Unstable" << std::endl;
+                    }
+                }
+            }
+        }
+    } 
+    createSimulation(7, 20);
+}
+
+
